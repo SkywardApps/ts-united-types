e0384ef017bb653cb4314a40498c1962f61e2ecc
Danny Calianno <dcalianno@skywardapps.com>
2023-03-27 11:07:21 -0400

    Export dynamic functions for public usage

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,8 @@
-import type { UnitedRatio } from './types/UnitedRatio';
 import type { UnitConversion } from './types/UnitConversion';
+import type { UnitedRatio } from './types/UnitedRatio';
 import type { UnitedScalar } from './types/UnitedScalar';
-import { ratio } from './ratio';
-import { scalar } from './scalar';
 import { converter } from './converter';
+import { ratio } from './ratio';
+import { dynamicToScalar, scalar, scalarToDynamic } from './scalar';
 
-export {ratio, scalar, converter, UnitedRatio, UnitConversion, UnitedScalar};
+export { converter, dynamicToScalar, ratio, scalar, scalarToDynamic, UnitConversion, UnitedRatio, UnitedScalar };
